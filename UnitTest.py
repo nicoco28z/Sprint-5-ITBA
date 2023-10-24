0655c0c221a11d0a6ef65042d90480e6aa48c337
nicoc <nicoco28z@gmail.com>
2023-10-24 08:54:45 -0300

    Update UnitTest.py

--- UnitTest.py
+++ UnitTest.py
@@ -14,7 +14,7 @@ if __name__ == "__main":
     unittest.main()
 
 
-def calcular_monto_total(precio_dolar, monto):
+def calcular_monto_total(monto):
     impuesto_pais = 0.25  # Impuesto 25%
     ganancias = 0.35  # Impuesto 35%
     monto_total = monto + (monto * impuesto_pais) + (monto * ganancias)
@@ -23,8 +23,12 @@ def calcular_monto_total(precio_dolar, monto):
 def descontar_comision(monto, comision_porcentaje):
     comision = monto * (comision_porcentaje / 100)
     monto_descontado = monto - comision
-    return monto_descontado
+    return monto_descontado , comision
 
 def calcular_monto_plazo_fijo(monto, interes):
     monto_final = monto + (monto * (interes / 100))
     return monto_final
+
+print(calcular_monto_total(50000))
+print(descontar_comision(50000, 1))
+print(calcular_monto_plazo_fijo(50000, 10))
