0884f6b4fdbb893304d68781f142b1f860d7e606
nicoc <nicoco28z@gmail.com>
2023-10-24 08:58:19 -0300

    Update UnitTest.py

--- UnitTest.py
+++ UnitTest.py
@@ -2,10 +2,10 @@ import unittest
 
 class TestFunciones(unittest.TestCase):
     def test_calcular_monto_total(self):
-        self.assertEqual(calcular_monto_total(100, 1000), 1650.0)
+        self.assertEqual(calcular_monto_total(1000), 1600.0)
 
     def test_descontar_comision(self):
-        self.assertEqual(descontar_comision(1000, 10), 900.0)
+        self.assertEqual(descontar_comision(1000, 10), (900.0, 100.0))
 
     def test_calcular_monto_plazo_fijo(self):
         self.assertEqual(calcular_monto_plazo_fijo(1000, 5), 1050.0)
@@ -29,6 +29,11 @@ def calcular_monto_plazo_fijo(monto, interes):
     monto_final = monto + (monto * (interes / 100))
     return monto_final
 
-print(calcular_monto_total(50000))
+print(calcular_monto_total(1000))
 print(descontar_comision(50000, 1))
 print(calcular_monto_plazo_fijo(50000, 10))
+
+test = TestFunciones()
+test.test_calcular_monto_plazo_fijo()
+test.test_calcular_monto_total()
+test.test_descontar_comision()
